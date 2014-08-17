05c1b43c81ccb9a4852fb66006f45bfa87b5c7d2
Christian Meier <m.kristian@web.de>
2014-08-17 18:32:10 +0200

    get legacy to work

--- src/main/java/de/saumya/mojo/rubygems/LegacyRubygemsServletContextListener.java
+++ src/main/java/de/saumya/mojo/rubygems/LegacyRubygemsServletContextListener.java
@@ -8,10 +8,8 @@ import java.net.URL;
 import org.sonatype.nexus.ruby.DefaultRubygemsGateway;
 import org.sonatype.nexus.ruby.RubygemsFile;
 import org.sonatype.nexus.ruby.RubygemsGateway;
-import org.sonatype.nexus.ruby.cuba.DefaultRubygemsFileSystem;
 import org.sonatype.nexus.ruby.cuba.RubygemsFileSystem;
 import org.sonatype.nexus.ruby.layout.CachingStorage;
-import org.sonatype.nexus.ruby.layout.ProxiedRubygemsFileSystem;
 import org.sonatype.nexus.ruby.layout.ProxyStorage;
 
 public class LegacyRubygemsServletContextListener extends AbstractRubygemsServletContextListener
@@ -53,7 +51,7 @@ public class LegacyRubygemsServletContextListener extends AbstractRubygemsServle
             throw new RuntimeException( "no storage path given");
         }
         ProxyStorage storage = new CachingStorage( path, new URL( "https://rubygems.org" ) );
-        RubygemsFileSystem rubygems = new ProxiedRubygemsFileSystem( gateway, storage );
+        RubygemsFileSystem rubygems = new LegacyRubygemsFileSystem( gateway, storage );
         configor.register( RubygemsFileSystem.class.getName(), rubygems );
     }
 
